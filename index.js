80abdb3b5b05003e802c3432659f86f5b4d6cbcc
Josh <87762857+radna0@users.noreply.github.com>
2023-01-26 07:24:37 +0700

    Done

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const app = require('./app')
 const { handleSchedule } = require('./utility')
 require('dotenv').config()
 
+async function getDataTZ() {
+  let response = await fetch(`${process.env.EXPRESS_ALL_TIMEZONES}`)
+  let data = await response.json()
+  return data
+}
 app.listen(process.env.EXPRESS_PORT, async () => {
   console.log(
     `💵 Server running and listening on http://localhost:${process.env.EXPRESS_PORT}/ …`
   )
-  async function getDataTZ() {
-    let response = await fetch(`${process.env.EXPRESS_ALL_TIMEZONES}`)
-    let data = await response.json()
-    return data
-  }
   const allTimeZones = await getDataTZ()
   for (let data in allTimeZones) {
     handleSchedule(data)
